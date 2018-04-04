cde5f6e97bd8b0a3b5a977eaf76c1eaefab27bd2
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-04-04 14:45:47 -0400

    add back compiled artifacts

---
+++ index.js
@@ -0,0 +1,9 @@
+import Clovers from "./build/contracts/Clovers.json";
+import CloversController from "./build/contracts/CloversController.json";
+import CloversMetadata from "./build/contracts/CloversMetadata.json";
+import CloversToken from "./build/contracts/CloversToken.json";
+import IClovers from "./build/contracts/IClovers.json";
+import ICloversController from "./build/contracts/ICloversController.json";
+import Reversi from "./build/contracts/Reversi.json";
+
+export { Clovers, CloversController, CloversMetadata, CloversToken, IClovers, ICloversController, Reversi };
