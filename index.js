f857f54c796aac590e98aeb24bccc7f6d8d77700
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-04-08 15:13:54 -0400

    es5

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-import Clovers from "./build/contracts/Clovers.json";
-import CloversController from "./build/contracts/CloversController.json";
-import CloversMetadata from "./build/contracts/CloversMetadata.json";
-import ClubToken from "./build/contracts/ClubToken.json";
-import IClovers from "./build/contracts/IClovers.json";
-import ICloversController from "./build/contracts/ICloversController.json";
-import Reversi from "./build/contracts/Reversi.json";
+var Clovers require("./build/contracts/Clovers.json");
+var CloversController require("./build/contracts/CloversController.json");
+var CloversMetadata require("./build/contracts/CloversMetadata.json");
+var ClubToken require("./build/contracts/ClubToken.json");
+var IClovers require("./build/contracts/IClovers.json");
+var ICloversController require("./build/contracts/ICloversController.json");
+var Reversi require("./build/contracts/Reversi.json");
 
-export { Clovers, CloversController, CloversMetadata, ClubToken, IClovers, ICloversController, Reversi };
+export {
+  Clovers:Clovers,
+  CloversController:CloversController,
+  CloversMetadata:CloversMetadata,
+  ClubToken:ClubToken,
+  IClovers:IClovers,
+  ICloversController:ICloversController,
+  Reversi:Reversi
+};
